4afc168f73f45238e0e27d5daa4b01a6763e4b38
Ajay <ajaykcoder@gmail.com>
2024-04-20 22:22:47 +0530

    changes

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
                     <h1 className="text-4xl font-medium text-darkBlack mb-5">Learn anywhere, Learn quickly.</h1>
                     <div className="text-white text-sm leading-6 max-w-[450px]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
                     <div className="flex gap-x-3 mt-7">
-                        <button className="text-base h-10 text-white bg-darkBlack hover:bg-black px-6 rounded">Get Started</button>
+                        <button className="text-base h-10 text-white bg-darkBlack hover:bg-transparent hover:text-darkBlack border-2 border-darkBlack px-6 rounded">Get Started</button>
                         <button className="text-base h-10 text-darkBlack bg-transparent hover:bg-darkBlack hover:text-white border-2 border-darkBlack px-6 rounded">About Us</button>
                     </div>
                 </div>
